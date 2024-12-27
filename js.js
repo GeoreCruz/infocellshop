43820fa763478bb3c57dbedee73a195c9b30fbad
GeorgeCruz <george.som@hotmail.com>
2024-12-27 11:02:19 -0300

    projeto iniciado

---
+++ js.js
@@ -0,0 +1,17 @@
+function foto01() {
+    document.getElementById('foto-destaque').src="img/fone-destaque01.png"
+    document.getElementById('titulo').innerHTML = 'Fone de ouvido sem fio JBL Tune 520BT Dobrável Preto'
+    document.querySelector('.price').innerHTML = 'R$ 227,69'
+}
+
+function foto02() {
+    document.getElementById('foto-destaque').src="img/fone-destaque02.png"
+    document.getElementById('titulo').innerHTML = 'Fone de ouvido Tune 520bt Branco Dobrável JBL'
+    document.querySelector('.price').innerHTML = 'R$ 212,90'
+}
+
+function foto03() {
+    document.getElementById('foto-destaque').src="img/fone-destaque03.png"
+    document.getElementById('titulo').innerHTML = 'Fone de ouvido sem fio JBL Tune 520BT azul'
+    document.querySelector('.price').innerHTML = 'R$ 220,99'
+}
